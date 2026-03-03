e99008ea65491ba466cbe87e6dfc5f875536035d
Fernando Halim <fernandohalim26@gmail.com>
2026-03-03 16:19:14 +0700

    review ids navigation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,32 +8,30 @@ import FadeUp from "@/utilities/FadeUp";
 export default function Home() {
   return (
     <main className="bg-black text-white selection:bg-green-500/30 overflow-hidden">
-      <div id="hero"></div>
+      <div id="hero" />
       <FadeUp delay={0.1}>
         <Hero />
       </FadeUp>
 
-      <div id="skills"></div>
+      <div id="skills" />
       <FadeUp delay={0.1}>
         <Skills />
       </FadeUp>
 
-      <div id="experience"></div>
+      <div id="experience" />
       <FadeUp delay={0.1}>
         <Experience />
       </FadeUp>
 
-      <div id="projects">
-        <FadeUp delay={0.1}>
-          <Projects />
-        </FadeUp>
-      </div>
+      <div id="projects" />
+      <FadeUp delay={0.1}>
+        <Projects />
+      </FadeUp>
 
-      <div id="footer">
-        <FadeUp delay={0.1}>
-          <Footer />
-        </FadeUp>
-      </div>
+      <div id="footer" />
+      <FadeUp delay={0.1}>
+        <Footer />
+      </FadeUp>
     </main>
   );
 }
